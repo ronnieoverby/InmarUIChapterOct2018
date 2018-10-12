81f1fab4e5e4138d15e25d0e182daf419cda4eb6
Ronnie Overby <rdodontplay@gmail.com>
2018-10-11 21:52:44 -0400

    created DrugSearchClient using fetch()

--- UIChapterOct2018/wwwroot/js/vue/app.js
+++ UIChapterOct2018/wwwroot/js/vue/app.js
@@ -1,1 +1,7 @@
-﻿
+﻿class DrugSearchClient {
+    static search(ndc) {
+        return fetch(`http://localhost:59467/ndc/${ndc}`)
+            .then(r => r.json())
+            .then(j => console.log(j));
+    }
+}
